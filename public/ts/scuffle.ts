e0388324e9d418d5ad5d606a4daab7757b89651c
Shockk <shokku.ra@gmail.com>
2014-03-29 23:18:45 +0000

    Changed https server to http but wrote ssl wrapper to allow secure connections

--- public/ts/scuffle.ts
+++ public/ts/scuffle.ts
@@ -1,5 +1,5 @@
 window.onload = () => {
-	var socket = io.connect('https://yellow.shockk.co.uk:1337')
+	var socket = io.connect('http://yellow.shockk.co.uk:1337')
 	socket.emit('name', 'shockk')
 	socket.on('hello', console.log.bind(console, 'Hello, %s!'))
 	var game = new Scuffle.Game(socket)
--- server/scuffled.ts
+++ server/scuffled.ts
@@ -1,12 +1,21 @@
 var fs = require('fs')
+var net = require('net')
+
+var app = require('http').createServer()
+var io = require('socket.io').listen(app)
+app.listen(1337)
+
 var opts = {
 	key:  fs.readFileSync(__dirname + '/ssl/server.key'),
 	cert: fs.readFileSync(__dirname + '/ssl/server.crt'),
 	ca:   fs.readFileSync(__dirname + '/ssl/ca.crt')
 }
-var app = require('https').createServer(opts)
-var io = require('socket.io').listen(app)
-app.listen(1337)
+require('tls').createServer(opts, stream => {
+	var req = net.connect({ port: 1337, host: '127.0.0.1'}, () => {
+		stream.pipe(req)
+		req.pipe(stream)
+	})
+}).listen(1338)
 
 io.sockets.on('connection', socket => {
 	socket.on('name', socket.emit.bind(socket, 'hello'))
