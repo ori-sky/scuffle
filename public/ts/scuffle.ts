ab18f9545b73152c8273777772e24c09bf621d0b
Shockk <shokku.ra@gmail.com>
2014-03-29 13:55:17 +0000

    Vastly cleaned up client code

--- public/ts/scuffle.ts
+++ public/ts/scuffle.ts
@@ -1,8 +1,6 @@
 window.onload = () => {
 	var socket = io.connect('https://yellow.shockk.co.uk:1337')
 	socket.emit('name', 'shockk')
-	socket.on('hello', (name : string) => {
-		console.log('Hello, ' + name + '!')
-	})
+	socket.on('hello', console.log.bind(console, 'Hello, %s!'))
 	var game = new Scuffle.Game(socket)
 }
