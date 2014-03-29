39655452f4336f5859214341ec7450c1b044028d
Shockk <shokku.ra@gmail.com>
2014-03-29 11:25:15 +0000

    Added socket.io.js script to index page, implemented (and forced) SSL for socket.io server

--- server/scuffled.ts
+++ server/scuffled.ts
@@ -1,4 +1,10 @@
-var app = require('http').createServer()
+var fs = require('fs')
+var opts = {
+	key:  fs.readFileSync(__dirname + '/ssl/server.key'),
+	cert: fs.readFileSync(__dirname + '/ssl/server.crt'),
+	ca:   fs.readFileSync(__dirname + '/ssl/ca.crt')
+}
+var app = require('https').createServer(opts)
 var io = require('socket.io').listen(app)
 app.listen(1337)
 
