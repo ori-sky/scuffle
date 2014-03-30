917d7029e5e301dd92638a6e870a7ff4fa692ecf
Shockk <shokku.ra@gmail.com>
2014-03-30 23:21:15 +0000

    Reconnect on disconnect

--- public/ts/scuffle.ts
+++ public/ts/scuffle.ts
@@ -1,4 +1,7 @@
 window.onload = () => {
 	var socket = io.connect('http://yellow.shockk.co.uk:1337')
+	socket.on('disconnect', () => {
+		io.connect('http://yellow.shockk.co.uk:1337')
+	})
 	var game = new Scuffle.Game(socket)
 }
