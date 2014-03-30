706478343883793a8fd6e55a32ea616cffc08589
Shockk <shokku.ra@gmail.com>
2014-03-30 09:51:29 +0000

    Added loading text, fixed artificial slowerdowner to work on latest stable Firefox

--- public/ts/PreloadState.ts
+++ public/ts/PreloadState.ts
@@ -1,19 +1,30 @@
+declare var WebFontConfig
+
 module Scuffle {
 	export class PreloadState extends Phaser.State {
-		bar : Phaser.Sprite
+		group : Phaser.Group
 
 		preload() {
-			this.bar = this.add.sprite(this.world.centerX - 400, this.world.centerY - 25, 'bar1')
-			this.bar.alpha = 0
-			var tween = this.add.tween(this.bar).to({ alpha: 1 }, 1000, Phaser.Easing.Linear.None, true);
-			this.load.setPreloadSprite(this.bar)
+			this.group = this.add.group()
+			this.group.alpha = 0
+			this.add.tween(this.group).to({ alpha: 1 }, 1000, Phaser.Easing.Linear.None, true)
+
+			var bar = this.add.sprite(this.world.centerX - 400, this.world.centerY + 60, 'bar1', 0, this.group)
+			this.load.setPreloadSprite(bar)
+
 			for(var i=2; i<50; ++i)
-				this.load.image('bar' + i, 'img/bar1.png')
+				this.load.image('bar' + i, 'img/bar' + i + '.png')
 			this.load.image('logo', 'img/phaser2.png')
+
+			var text = this.add.text(this.world.centerX, this.world.centerY, 'Loading', undefined, this.group)
+			text.anchor.setTo(0.5, 0.5)
+			text.font = 'Iceland'
+			text.fontSize = 60
+			text.fill = '#acf'
 		}
 
 		create() {
-			var tween = this.add.tween(this.bar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
+			var tween = this.add.tween(this.group).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true)
 		}
 	}
 }
