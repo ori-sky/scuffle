b9f7060bf0a046e11a0ab0521c6187cd5fd8de26
Shockk <shokku.ra@gmail.com>
2014-03-30 15:12:03 +0000

    Wrote constructors for common classes

--- common/Map.ts
+++ common/Map.ts
@@ -2,15 +2,34 @@ module Scuffle {
 	export class Point {
 		x : number
 		y : number
+
+		constructor(x : number, y : number) {
+			this.x = x
+			this.y = y
+		}
 	}
 
 	export class Line {
 		a : Point
 		b : Point
+
+		constructor(a : Point, b : Point) {
+			this.a = a
+			this.b = b
+		}
 	}
 
 	export class Map {
 		name : string
 		lines : Line[]
+
+		constructor(name : string, lines : Line[] = []) {
+			this.name = name
+			this.lines = lines
+		}
+
+		pushLine(line : Line) {
+			this.lines.push(line)
+		}
 	}
 }
