96c0d3597a7dd2d2e29d6d605632eeef06254822
Shockk <shokku.ra@gmail.com>
2014-03-30 17:39:32 +0000

    Tween alpha 0 to 1 for map lines

--- public/ts/MapState.ts
+++ public/ts/MapState.ts
@@ -2,6 +2,7 @@ module Scuffle {
 	export class MapState extends Phaser.State {
 		game : Game
 		mapName : string
+		group : Phaser.Group
 
 		init(name : string) {
 			if(typeof name !== 'string')
@@ -10,16 +11,20 @@ module Scuffle {
 		}
 
 		preload() {
-
+			this.group = this.add.group()
 		}
 
 		create() {
-			var graphics = this.add.graphics(0, 0)
-			graphics.lineStyle(5, 0xffd900, 1)
 
 			this.game.socket.emit('map.get', this.mapName)
 			this.game.socket.on('map', (map : Scuffle.Map) => {
 				map.lines.forEach(line => {
+					var group = this.add.group(this.group)
+					group.alpha = 0
+					this.add.tween(group).to({ alpha: 1 }, 400, Phaser.Easing.Linear.None, true)
+
+					var graphics = this.add.graphics(0, 0, group)
+					graphics.lineStyle(5, 0xffd900, 1)
 					graphics.moveTo(line.a.x, line.a.y)
 					graphics.lineTo(line.b.x, line.b.y)
 				})
