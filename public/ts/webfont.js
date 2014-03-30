019dfa0f8c2e472dda44ed94d9a2caf019e9e3c2
Shockk <shokku.ra@gmail.com>
2014-03-30 09:49:35 +0000

    Wrote webfont.js

---
+++ public/ts/webfont.js
@@ -0,0 +1,19 @@
+
+WebFontConfig = {
+	google: {
+		families: [
+			'Iceland::latin',
+			'VT323::latin'
+		]
+	}
+};
+
+(function() {
+	var wf = document.createElement('script')
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http')
+		+ '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
+	wf.type = 'application/javascript'
+	wf.async = false
+	var s = document.getElementsByTagName('script')[0]
+	s.parentNode.insertBefore(wf, s)
+})()
