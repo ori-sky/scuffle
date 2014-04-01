dd9dd38767a45379521dd9f9bf1d65e4ef485fca
Shockk <shokku.ra@gmail.com>
2014-04-01 12:25:28 +0000

    Implemented map sprites, changed color of map lines

--- public/ts/MapState.ts
+++ public/ts/MapState.ts
@@ -13,13 +13,18 @@ module Scuffle {
 		}
 
 		create() {
+			this.map.sprites.forEach(sprite => {
+				var image : any = this.cache.getImage(sprite.source)
+				var s = this.add.sprite(sprite.pos.x, sprite.pos.y, sprite.source, 0, this.group)
+				s.scale.setTo(sprite.size.x / image.width, sprite.size.y / image.height)
+			})
 			this.map.lines.forEach(line => {
 				var group = this.add.group(this.group)
 				group.alpha = 0
 				this.add.tween(group).to({ alpha: 1 }, 400, Phaser.Easing.Linear.None, true)
 
 				var graphics = this.add.graphics(0, 0, group)
-				graphics.lineStyle(5, 0xffd900, 1)
+				graphics.lineStyle(3, 0xffffff, 1)
 				graphics.moveTo(line.a.x, line.a.y)
 				graphics.lineTo(line.b.x, line.b.y)
 			})
