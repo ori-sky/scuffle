54a54a0facba45eba8d715666c5b7f9d002ee465
Shockk <shokku.ra@gmail.com>
2014-04-02 12:32:05 +0000

    Wrote constructor and add method for common Point class

--- common/Point.ts
+++ common/Point.ts
@@ -2,5 +2,15 @@ module Scuffle {
 	export class Point {
 		x : number
 		y : number
+
+		constructor(x : number, y : number) {
+			this.x = x
+			this.y = y
+		}
+
+		add(v : Point) {
+			this.x += v.x
+			this.y += v.y
+		}
 	}
 }
