f97eda8a662880327a2a529837f09332ae4d72a5
Shockk <shokku.ra@gmail.com>
2014-04-15 21:15:48 +0000

    Added bullet velocity

--- common/Bullet.ts
+++ common/Bullet.ts
@@ -2,14 +2,16 @@ module Scuffle {
 	export class Bullet {
 		id : number
 		owner : number
-		pos : Point
 		radius : number
+		pos : Point
+		velocity : Point
 
 		constructor(id : number, owner : number) {
 			this.id = id
 			this.owner = owner
-			this.pos = new Point(0, 0)
 			this.radius = 1
+			this.pos = new Point(0, 0)
+			this.velocity = new Point(0, 0)
 		}
 	}
 }
