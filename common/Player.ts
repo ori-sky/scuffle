6d995dccefc6dcd38cc72a89f9f0361dc7f15d73
Shockk <shokku.ra@gmail.com>
2014-04-15 04:58:47 +0000

    Added angle to Player

--- common/Player.ts
+++ common/Player.ts
@@ -3,11 +3,13 @@ module Scuffle {
 		id : number
 		pos : Point
 		velocity : Point
+		angle : number
 
 		constructor(id : number) {
 			this.id = id
 			this.pos = new Point(0, 0)
 			this.velocity = new Point(0, 0)
+			this.angle = 0
 		}
 	}
 }
