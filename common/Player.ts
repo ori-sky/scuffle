672f226d064d083d1a51afb5c78ec5491094c515
Shockk <shokku.ra@gmail.com>
2014-04-17 23:44:54 +0000

    Added fields to Player

--- common/Player.ts
+++ common/Player.ts
@@ -1,12 +1,18 @@
 module Scuffle {
 	export class Player {
 		id : number
+		color : number
+		alpha : number
+		radius : number
 		pos : Point
 		velocity : Point
 		angle : number
 
 		constructor(id : number) {
 			this.id = id
+			this.color = 0xffffff
+			this.alpha = 1
+			this.radius = 7
 			this.pos = new Point(0, 0)
 			this.velocity = new Point(0, 0)
 			this.angle = 0
