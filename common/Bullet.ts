03df7d527482aaa1353d39f2babd1843c2059b26
Shockk <shokku.ra@gmail.com>
2014-04-17 22:32:04 +0000

    Added bullet color field

--- common/Bullet.ts
+++ common/Bullet.ts
@@ -2,6 +2,7 @@ module Scuffle {
 	export class Bullet {
 		id : number
 		owner : number
+		color : number
 		radius : number
 		pos : Point
 		velocity : Point
@@ -9,6 +10,7 @@ module Scuffle {
 		constructor(id : number, owner : number) {
 			this.id = id
 			this.owner = owner
+			this.color = 0xffffff
 			this.radius = 1
 			this.pos = new Point(0, 0)
 			this.velocity = new Point(0, 0)
