0a9df8e3153ef4f712db3be4f702657ef278bf23
Shockk <shokku.ra@gmail.com>
2014-04-17 23:26:04 +0000

    Added bullet alpha field

--- common/Bullet.ts
+++ common/Bullet.ts
@@ -3,6 +3,7 @@ module Scuffle {
 		id : number
 		owner : number
 		color : number
+		alpha : number
 		radius : number
 		pos : Point
 		velocity : Point
@@ -11,6 +12,7 @@ module Scuffle {
 			this.id = id
 			this.owner = owner
 			this.color = 0xffffff
+			this.alpha = 1
 			this.radius = 1
 			this.pos = new Point(0, 0)
 			this.velocity = new Point(0, 0)
