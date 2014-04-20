546b343a72ba97c9b136da44349f748174b3a757
Shockk <shokku.ra@gmail.com>
2014-04-20 17:36:28 +0000

    Wrote circlesIntersect method at top level of Scuffle module

---
+++ common/Helper.ts
@@ -0,0 +1,6 @@
+module Scuffle {
+	export function circlesIntersect(p1 : Point, r1 : number, p2 : Point, r2 : number) {
+		var v = Point.prototype.addedTo.call(p2, Point.prototype.scaledBy.call(p1, -1))
+		return v.length() < r1 + r2
+	}
+}
