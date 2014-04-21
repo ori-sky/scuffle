2bf2ca58928891bb83c25b46640cc0ec4f65cd27
Shockk <shokku.ra@gmail.com>
2014-04-21 01:53:22 +0000

    Wrote Bullet object compression methods

--- common/Bullet.ts
+++ common/Bullet.ts
@@ -19,5 +19,32 @@ module Scuffle {
 			this.velocity = new Point(0, 0)
 			this.damage = 20
 		}
+
+		compress(quality? : number) {
+			var obj = [this.id, this.owner, this.color]
+			if(quality !== undefined)
+				obj.push(+this.alpha.toFixed(quality), +this.radius.toFixed(quality))
+			else
+				obj.push(this.alpha, this.radius)
+			obj.push(Point.prototype.compress.call(this.pos, quality))
+			obj.push(Point.prototype.compress.call(this.velocity, quality))
+			if(quality !== undefined)
+				obj.push(+this.damage.toFixed(quality))
+			else
+				obj.push(this.damage)
+			return obj
+		}
+
+		static uncompress(obj : any) {
+			expectLength(obj, 8)
+			var bullet = new Bullet(obj[0], obj[1])
+			bullet.color = obj[2]
+			bullet.alpha = obj[3]
+			bullet.radius = obj[4]
+			bullet.pos = Point.uncompress(obj[5])
+			bullet.velocity = Point.uncompress(obj[6])
+			bullet.damage = obj[7]
+			return bullet
+		}
 	}
 }
