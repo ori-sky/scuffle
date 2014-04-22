108279492c6aedbfcff178c81911242e555b85dd
Shockk <shokku.ra@gmail.com>
2014-04-22 04:18:02 +0000

    Added syncState to Scuffle.Game

--- public/ts/Game.ts
+++ public/ts/Game.ts
@@ -1,9 +1,11 @@
 module Scuffle {
 	export class Game extends Phaser.Game {
 		socket : Socket
+		syncState : { [ k : string] : boolean }
 
 		constructor() {
 			super(1280, 720, Phaser.AUTO, 'content', null)
+			this.syncState = {}
 
 			this.state.add('Boot', BootState, true)
 			this.state.add('Preload', PreloadState)
