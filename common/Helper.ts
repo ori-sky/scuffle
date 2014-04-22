eb213f85c575b093d091e20d7c6809a21fcb2f7c
Shockk <shokku.ra@gmail.com>
2014-04-22 05:06:07 +0000

    Wrote tickPlayerVelocity helper method

--- common/Helper.ts
+++ common/Helper.ts
@@ -25,4 +25,31 @@ module Scuffle {
 		}
 		return intersects ? intersects : circlesIntersect(b, r1, p, r2)
 	}
+
+	export function tickPlayerVelocity(time : number, state : any, player : Player) {
+			var moveVector = new Point(0, 0)
+			if(state['key.a'] || state['key.left'])
+				moveVector.add(-1,  0)
+			if(state['key.d'] || state['key.right'])
+				moveVector.add( 1,  0)
+			if(state['key.w'] || state['key.up'])
+				moveVector.add( 0, -1)
+			if(state['key.s'] || state['key.down'])
+				moveVector.add( 0,  1)
+
+			var vel = Point.prototype.copy.call(player.velocity)
+			if(!moveVector.isZero()) {
+				moveVector.normalize()
+				moveVector.scale(0.0025 * time)
+				if(state['key.shift'])
+					moveVector.scale(1 / 2)
+				vel.addPoint(moveVector)
+			}
+
+			vel.scale(1 - 0.011 * time)
+			if(vel.length() < 0.005)
+				vel.zero()
+
+			return (player.velocity = vel)
+	}
 }
