972e53c240874cd3546ebb03adddfbbb63b23481
Shockk <shokku.ra@gmail.com>
2014-04-23 02:47:32 +0000

    Made bullets spawn at edge of player rather than in middle

--- server/Client.ts
+++ server/Client.ts
@@ -98,11 +98,12 @@ module Scuffle {
 							0xaaccff
 						]
 						bullet.color = colors[Math.floor(Math.random() * colors.length)]
-						bullet.pos = Point.prototype.copy.call(this.player.pos)
 						var angle = this.player.angle// + (Math.random() - 0.5) / 10
 						bullet.velocity.x = Math.cos(angle)
 						bullet.velocity.y = Math.sin(angle)
 						bullet.velocity.scale(0.5)
+						bullet.pos = Point.prototype.addedTo.call(this.player.pos,
+										bullet.velocity.x * this.player.radius, bullet.velocity.y * this.player.radius)
 						this.game.io.sockets.in(this.instance.id).emit(50, bullet.compress(4))
 						this.accumBullet = 0
 					}
